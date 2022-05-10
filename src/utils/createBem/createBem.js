1e0625561967bff5012057d712e29a3e2e4135f1
miko-github <mikoloism-github@gmail.com>
2022-05-10 19:51:52 +0430

    refactor (utils, bem) : "createBem" refactored to if statement

--- src/utils/createBem/createBem.js
+++ src/utils/createBem/createBem.js
@@ -2,7 +2,11 @@
 
 function createBem(block) {
 	function useElement(element, modifier) {
-		if (element && !modifier) {
+		if (element) {
+			if (modifier) {
+				return `${block}__${element}--${modifier}`;
+			}
+
 			return `${block}__${element}`;
 		}
 
