7d516adba1ef2b0c9d31b6651f41254ed7c84777
miko-github <mikoloism-github@gmail.com>
2022-05-10 20:25:01 +0430

    test (navigate, button) : adding navigate-[right, left] stories

---
+++ src/stories/NavigateLeft.stories.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { NavigateLeft } from './../components/Button';
+
+export default {
+	title: 'Atoms/Button/NavigateLeft',
+	component: NavigateLeft,
+};
+
+const Template = (args) => <NavigateLeft {...args} />;
+
+export const index = Template.bind({});
---
+++ src/stories/NavigateRight.stories.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { NavigateRight } from './../components/Button';
+
+export default {
+	title: 'Atoms/Button/NavigateRight',
+	component: NavigateRight,
+};
+
+const Template = (args) => <NavigateRight {...args} />;
+
+export const index = Template.bind({});
