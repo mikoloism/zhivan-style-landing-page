d2f6e6c80acfa0efc2c948ff67cbdf795d3e3c50
miko-github <mikoloism-github@gmail.com>
2022-05-10 20:20:56 +0430

    chor (icon) : adding 'chevron-left' icon

--- src/components/Icon/index.js
+++ src/components/Icon/index.js
@@ -1,3 +1,4 @@
 export { default as ICart } from './ICart';
 export { default as ISearch } from './ISearch';
 export { default as IChevronRight } from './IChevronRight';
+export { default as IChevronLeft } from './IChevronLeft';
