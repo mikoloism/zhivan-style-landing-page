872ae3bfb6de33a6e76ee49edfa6364c2956109f
miko-github <mikoloism-github@gmail.com>
2022-05-10 19:19:46 +0430

    test (button) : seprate cart and buy

---
+++ src/stories/Cart.stories.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Cart } from '../components/Button';
+
+export default {
+	title: 'Atoms/Button/Cart',
+	component: Cart,
+};
+
+const Template = (args) => <Cart {...args} />;
+
+export const index = Template.bind({});
