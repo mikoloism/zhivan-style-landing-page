a7af72cdfb9a7d6eb23b72e0c603b9e0a7f8f172
miko-github <mikoloism-github@gmail.com>
2022-05-11 14:32:16 +0430

    chor (image, components) : create favorites image wrapper component

---
+++ src/components/Image/Image.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+import { createBem } from './../../utils';
+import image from './../../assets/images/image-style(3).png';
+
+function Picture() {
+	return (
+		<picture>
+			<img src={image} alt='style | zhivan style' />
+		</picture>
+	);
+}
+
+function Image({ src, alt, className }) {
+	const cover = createBem('cover');
+
+	return (
+		<figure className={classnames(className?.figure, cover())}>
+			<img
+				className={classnames(className?.img, cover('image'))}
+				src={src}
+				alt={alt}
+			/>
+		</figure>
+	);
+}
+
+Image.propTypes = {
+	src: PropTypes.oneOfType([
+		PropTypes.string,
+		PropTypes.elementType,
+		PropTypes.element,
+	]),
+	alt: PropTypes.string,
+	className: PropTypes.oneOfType([
+		PropTypes.string,
+		PropTypes.shape({
+			figure: PropTypes.string,
+			img: PropTypes.string,
+		}),
+	]),
+};
+
+Image.defaultProps = {
+	src: image,
+	alt: 'zhivan style | clothes code #01',
+};
+
+export default Image;
+export { Picture };
