04751141f9ef0c619c509de354324da10e6b4b02
miko-github <mikoloism-github@gmail.com>
2022-05-12 19:59:04 +0430

    test (headline, storybook) : update the type of more story in Headline as Type Arguments

--- src/stories/Headline.stories.jsx
+++ src/stories/Headline.stories.jsx
@@ -8,9 +8,23 @@ export default {
 
 const Template = (args) => <Headline {...args} />;
 
-export const H1 = Template.bind({});
-H1.args = {
-	type: 'h1',
-	title: 'محبوب ترین های ماه',
+export const H2 = Template.bind({});
+H2.args = {
+	type: 'h2',
+	title: ' !!! الان بخــــر، شش مــاه دیـگــه پولشـــو بــده',
+	description: 'Payment after 6 months',
+};
+
+export const H3 = Template.bind({});
+H3.args = {
+	type: 'h3',
+	title: 'دستـه بنـدی ها',
 	description: 'most popular of the month',
+};
+
+export const H4 = Template.bind({});
+H4.args = {
+	type: 'h4',
+	title: 'تونیک زنانه گلدار',
+	description: "Floral women's tonic",
 };
