cca184504941e601d11bab81056d16d9fc3b40b4
miko-github <mikoloism-github@gmail.com>
2022-05-12 19:38:13 +0430

    refactor (components, headline) : use functional components instead of classic components

--- src/components/Headline/Headline.jsx
+++ src/components/Headline/Headline.jsx
@@ -2,71 +2,30 @@ import React from 'react';
 import classnames from 'classnames';
 import { createBem } from './../../utils';
 
-class Headline extends React.Component {
-	constructor(props) {
-		super();
-		this.props = props;
-		this.state = {};
-		this.heading = createBem('headline');
-	}
+const heading = createBem('headline');
 
-	renderH1(text) {
-		return (
-			<h1 className={classnames(this.heading('title', 'h1'))}>{text}</h1>
-		);
-	}
-	renderH2(text) {
-		return (
-			<h2 className={classnames(this.heading('title', 'h2'))}>{text}</h2>
-		);
-	}
-	renderH3(text) {
-		return (
-			<h3 className={classnames(this.heading('title', 'h3'))}>{text}</h3>
-		);
-	}
-	renderH4(text) {
-		return (
-			<h4 className={classnames(this.heading('title', 'h4'))}>{text}</h4>
-		);
-	}
-	renderH5(text) {
-		return (
-			<h5 className={classnames(this.heading('title', 'h5'))}>{text}</h5>
-		);
-	}
-	renderH6(text) {
-		return (
-			<h6 className={classnames(this.heading('title', 'h6'))}>{text}</h6>
-		);
-	}
-	renderTitle(type, text) {
-		return [
-			this.renderH1,
-			this.renderH2,
-			this.renderH3,
-			this.renderH4,
-			this.renderH5,
-			this.renderH6,
-		][parseInt(type) || 0](text);
-	}
+function RenderTitle({ type, text }) {
+	return {
+		h1: <h1 className={classnames(heading('title', 'h1'))}>{text}</h1>,
+		h2: <h2 className={classnames(heading('title', 'h2'))}>{text}</h2>,
+		h3: <h3 className={classnames(heading('title', 'h3'))}>{text}</h3>,
+		h4: <h4 className={classnames(heading('title', 'h4'))}>{text}</h4>,
+		h5: <h5 className={classnames(heading('title', 'h5'))}>{text}</h5>,
+		h6: <h6 className={classnames(heading('title', 'h6'))}>{text}</h6>,
+	}[type || 'h1'];
+}
 
-	renderDescription(description) {
-		return (
-			<span className={classnames(this.heading('description'))}>
-				{description}
-			</span>
-		);
-	}
+function RenderDescription({ text }) {
+	return <span className={classnames(heading('description'))}>{text}</span>;
+}
 
-	render() {
-		return (
-			<section className={classnames(this.heading())}>
-				{this.renderTitle(this.props.type, this.props.title)}
-				{this.renderDescription(this.props.description)}
-			</section>
-		);
-	}
+function Headline({ type, title, description }) {
+	return (
+		<section className={classnames(heading())}>
+			<RenderTitle type={type} text={title} />
+			<RenderDescription text={description} />
+		</section>
+	);
 }
 
 export default Headline;
