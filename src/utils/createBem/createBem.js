c4848fb32db883ca1988173cd7aed0d35ec144c9
miko-github <mikoloism-github@gmail.com>
2022-05-12 19:37:14 +0430

    fix (create-bem, utils) : return both block-element and block-element-modifier when element and modifier passed as arguments

--- src/utils/createBem/createBem.js
+++ src/utils/createBem/createBem.js
@@ -4,7 +4,7 @@ function createBem(block) {
 	function useElement(element, modifier) {
 		if (element) {
 			if (modifier) {
-				return `${block}__${element}--${modifier}`;
+				return `${block}__${element} ${block}__${element}--${modifier}`;
 			}
 
 			return `${block}__${element}`;
