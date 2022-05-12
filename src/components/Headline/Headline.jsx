75132eaa0fbe7cd44ea6f6de111a398f91d164df
miko-github <mikoloism-github@gmail.com>
2022-05-12 19:57:31 +0430

    fix (headline, components) : type modifier in bem class names adding in the headline block instead headline elements as root

--- src/components/Headline/Headline.jsx
+++ src/components/Headline/Headline.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import { createBem } from './../../utils';
 
@@ -6,12 +7,12 @@ const heading = createBem('headline');
 
 function RenderTitle({ type, text }) {
 	return {
-		h1: <h1 className={classnames(heading('title', 'h1'))}>{text}</h1>,
-		h2: <h2 className={classnames(heading('title', 'h2'))}>{text}</h2>,
-		h3: <h3 className={classnames(heading('title', 'h3'))}>{text}</h3>,
-		h4: <h4 className={classnames(heading('title', 'h4'))}>{text}</h4>,
-		h5: <h5 className={classnames(heading('title', 'h5'))}>{text}</h5>,
-		h6: <h6 className={classnames(heading('title', 'h6'))}>{text}</h6>,
+		h1: <h1 className={classnames(heading('title'))}>{text}</h1>,
+		h2: <h2 className={classnames(heading('title'))}>{text}</h2>,
+		h3: <h3 className={classnames(heading('title'))}>{text}</h3>,
+		h4: <h4 className={classnames(heading('title'))}>{text}</h4>,
+		h5: <h5 className={classnames(heading('title'))}>{text}</h5>,
+		h6: <h6 className={classnames(heading('title'))}>{text}</h6>,
 	}[type || 'h1'];
 }
 
@@ -21,11 +22,21 @@ function RenderDescription({ text }) {
 
 function Headline({ type, title, description }) {
 	return (
-		<section className={classnames(heading())}>
+		<section className={classnames(heading(null, type))}>
 			<RenderTitle type={type} text={title} />
 			<RenderDescription text={description} />
 		</section>
 	);
 }
+
+Headline.propTypes = {
+	type: PropTypes.oneOf(['h1', 'h2', 'h3', 'h4', 'h5', 'h6']),
+	title: PropTypes.string,
+	description: PropTypes.string,
+};
+
+Headline.defaultProps = {
+	type: 'h3',
+};
 
 export default Headline;
