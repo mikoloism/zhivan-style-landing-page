aca5c5bdc66bda55db63e862515c6094b918e462
miko-github <mikoloism-github@gmail.com>
2022-05-12 20:57:17 +0430

    chor (button, components) : export MoreButton as More

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -2,5 +2,6 @@ import './Button.scss';
 
 export { default as Cart } from './Cart';
 export { default as Buy } from './Buy';
+export { default as More } from './More';
 export { default as NavigateRight } from './NavigateRight';
 export { default as NavigateLeft } from './NavigateLeft';
