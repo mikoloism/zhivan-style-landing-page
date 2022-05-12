27f26bd1031b3691593c04f9d905272c9dc97a3f
miko-github <mikoloism-github@gmail.com>
2022-05-12 18:47:50 +0430

    fix (button , cart) : remove the 'this' keyword of class component attribute (props)

--- src/components/Button/Cart.jsx
+++ src/components/Button/Cart.jsx
@@ -3,12 +3,10 @@ import classnames from 'classnames';
 import { ICart } from '../Icon';
 import { createBem } from './../../utils';
 
-function CartButton({ className, click }) {
+function CartButton({ className, click, url }) {
 	const cart = createBem('cart');
 	return (
-		<a
-			href={this.props.url || '#'}
-			className={classnames(cart(), this.props.className)}>
+		<a href={url || '#'} className={classnames(cart(), className)}>
 			<ICart className={cart('icon')} />
 		</a>
 	);
