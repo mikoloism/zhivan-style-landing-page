d09747f6723ed23f426d2711dece6a4f7f654638
miko-github <mikoloism-github@gmail.com>
2022-05-27 23:37:07 +0430

    chor (header, components) : adding Header components

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { createBem } from './../../utils';
+
+import Avatar from './../Avatar';
+import Navigation from './../Navigation';
+import SearchBox from './../SearchBox';
+import { Cart } from './../Button';
+
+function Header() {
+	const header = createBem('header');
+	return (
+		<header className={header}>
+			<section className={header('center')}>
+				<h1 className={header('title')}>ZHIVAN STYLE</h1>
+				<Navigation className={header('navigation')} />
+			</section>
+			<section className={header('left')}>
+				<Cart className={header('cart')} />
+				<SearchBox className={header('searchbox')} />
+			</section>
+			<section className={header('right')}>
+				<Avatar className={header('avatar')} />
+			</section>
+		</header>
+	);
+}
+
+export default Header;
