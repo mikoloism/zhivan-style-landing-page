b0e9162a30a852daf033742aaeef2787f4b3c6dd
miko-github <mikoloism-github@gmail.com>
2022-06-03 12:42:15 +0430

    fix (Favorite Section) : replace Image component by ProductCard component and putting the items into the arrays

--- src/components/FavoriteSection/FavoriteSection.jsx
+++ src/components/FavoriteSection/FavoriteSection.jsx
@@ -3,7 +3,7 @@ import { createBem } from './../../utils';
 
 import { More } from './../Button';
 import Headline from './../Headline';
-import Image from '../Image';
+import Product from '../ProductCard';
 
 import image1 from './../../assets/images/image-style(1).png';
 import image2 from './../../assets/images/image-style(2).png';
@@ -12,6 +12,27 @@ import image3 from './../../assets/images/image-style(3).png';
 function Horizontally() {
 	return <span className='line'></span>;
 }
+
+const API_RES = [
+	{
+		thumbnail: image1,
+		name: 'تونیک زنانه گلدار',
+		description: "Floral women's tonic",
+		id: 1,
+	},
+	{
+		thumbnail: image2,
+		name: 'تونیک حریر گلدار',
+		description: 'Floral silk tonic',
+		id: 2,
+	},
+	{
+		thumbnail: image3,
+		name: 'تونیک حریر گلدار',
+		description: 'Floral silk tonic',
+		id: 3,
+	},
+];
 
 function FavoriteSection({ className }) {
 	const fav = createBem('favorite');
@@ -25,9 +46,18 @@ function FavoriteSection({ className }) {
 				description='most popular of the month'
 			/>
 			<section className={fav('images')}>
-				<Image src={image1} />
-				<Image src={image2} />
-				<Image src={image3} />
+				{API_RES.map((res) => {
+					return (
+						<Product
+							className={fav('product')}
+							thumbnail={res.thumbnail}
+							name={res.name}
+							description={res.description}
+							key={res.id}
+							id={res.id}
+						/>
+					);
+				})}
 			</section>
 		</section>
 	);
