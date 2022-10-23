4dd2288230d5f22c37b1cc4aaa4bc5c2cb82f1ff
mikoloism <mikoloism@gmail.com>
2022-10-23 17:20:40 +0330

    chore(config): add initiial configs to `next.config.js`;

--- next.config.js
+++ next.config.js
@@ -1,7 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+	reactStrictMode: true,
+	swcMinify: true,
+	trailingSlash: undefined,
+	basePath: '/zhivan-style-landing-page',
+	// TODO : add `images: { loader: 'custom' },`
+};
 
-module.exports = nextConfig
+/**
+ * TODO : uncomment pwa codes
+ * const withPWA = require('next-pwa');
+ * const runtimeCaching = require('next-pwa/cache');
+ *
+ * module.exports = withPWA({
+ *  pwa: {
+ *    dest: 'public',
+ *    runtimeCaching,
+ *   },
+ *  ...nextConfig,
+ * });
+ */
+
+module.exports = nextConfig;
