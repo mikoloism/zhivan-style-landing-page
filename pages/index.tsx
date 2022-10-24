29ef55a20ab770b158ce066ec2ef410cbf01c740
mikoloism <mikoloism@gmail.com>
2022-10-24 15:38:34 +0330

    chore(page): initial `primary navigation` value object in `index.tsx` page;

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,16 @@ import React from 'react';
 function getProps() {
     return {
         title: 'Zhivan Style Landing Page | MIKOLOISM',
+        navigation: {
+            primary: [
+                { url: '#', label: 'nav-1' },
+                { url: '#', label: 'nav-2' },
+                { url: '#', label: 'nav-3' },
+                { url: '#', label: 'nav-4' },
+                { url: '#', label: 'nav-5' },
+                { url: '#', label: 'nav-6' },
+            ],
+        },
     };
 }
 
