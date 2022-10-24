0a3039e051b7b397a1b773ea0d0e367d85891ca9
mikoloism <mikoloism@gmail.com>
2022-10-24 09:50:24 +0330

    feat(util): re-exported commonly types and modules from `utils/index.tsx`;

---
+++ utils/index.tsx
@@ -0,0 +1,8 @@
+export { default as classnames } from 'classnames';
+export { default as Image } from 'next/image';
+export { default as Link } from 'next/link';
+export { Component } from 'react';
+
+export type PropsWithClassName<T extends any   = {}> = T & { className?: string };
+export type PropsWithChildren<T extends any    = {}> = T & React.PropsWithChildren;
+export type PropsWithAllTogether<T extends any = {}> = T & PropsWithClassName & PropsWithChildren;
