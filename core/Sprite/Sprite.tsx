a1f000a3ea3fd9a9f52ecedf43e993fd9b080032
mikoloism <mikoloism@gmail.com>
2022-10-26 11:03:26 +0330

    feat(core): add `sprite` component to `core/` components;
    this component create re-usable way to access to `svg sprites assets` files and symbols by `<use />` tag;

---
+++ core/Sprite/Sprite.tsx
@@ -0,0 +1,35 @@
+import { Component } from 'utils';
+
+type Props = {
+    //* file-name
+    load: string;
+
+    //* path-name
+    name: string;
+};
+
+export class Sprite extends Component<Props> {
+    public constructor(props: Props) {
+        super(props);
+    }
+
+    public render() {
+        const symbolPath = this.generatePathToSymbol();
+
+        return (
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                xmlnsXlink="http://www.w3.org/1999/xlink"
+                role="img">
+                <use
+                    href={symbolPath}
+                    xlinkHref={symbolPath}
+                />
+            </svg>
+        );
+    }
+
+    private generatePathToSymbol() {
+        return `/zhivan-style-landing-page/assets/${this.props.load}-sprites.svg#${this.props.name}`;
+    }
+}
