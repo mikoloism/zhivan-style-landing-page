c1cc8ec05e2abd6cf783a916c9f7d46e156d3bce
mikoloism <mikoloism@gmail.com>
2022-10-27 16:44:19 +0330

    feat(core): add `Text` core component;

--- core/Text/Text.tsx
+++ core/Text/Text.tsx
@@ -1,7 +1,66 @@
-import { Component } from 'utils';
+import {
+    classnames,
+    Component,
+    createReactElement,
+    PropsWithAllTogether,
+    PropsWithChildren,
+    PropsWithClassName,
+} from 'utils';
+import style from './text.module.scss';
 
-export class TextComponent extends Component {
-    public render() {
-        return <></>;
+export class TextComponent extends Component<Props> {
+    public constructor(props: Props) {
+        super(props);
     }
+
+    public render() {
+        return this.generateTextElement(this.props.tag, {
+            className: this.createClassName(),
+            children: this.props.children,
+        });
+    }
+
+    private generateTextElement(
+        tagName: TagName,
+        { className, children }: PropsWithAllTogether
+    ) {
+        return createReactElement(tagName, { className }, children);
+    }
+
+    private createClassName(): string {
+        return classnames(
+            style[`text-${this.props.size}`],
+            style[`weight-${this.props.weight}`],
+            this.props.className
+        );
+    }
+
+    public static defaultProps: Omit<Props, 'children'> = {
+        tag: 'span',
+        size: 'md',
+        weight: 'medium',
+    };
 }
+
+type Props = PropsWithClassName<Required<PropsWithChildren>> & {
+    tag: TagName;
+    size: TextSize;
+    weight: TextWeight;
+};
+
+type TagName = HeadingTagName | StandardTagName;
+type HeadingTagName = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
+type StandardTagName = 'span' | 'p' | 'strong' | 'em';
+
+type TextSize =
+    | 'xs'
+    | 'sm'
+    | 'md'
+    | 'lg'
+    | 'xl'
+    | '2xl'
+    | '3xl'
+    | '4xl'
+    | '5xl';
+
+type TextWeight = 'light' | 'regular' | 'medium' | 'bold' | 'heavy' | 'fat';
