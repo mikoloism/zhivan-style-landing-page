1e6e97a9c53ccbf532fa39fef2cbd4ab8407b3ac
mikoloism <mikoloism@gmail.com>
2022-10-27 17:32:32 +0330

    feat(core): add `image` core component;

--- core/Image/Image.tsx
+++ core/Image/Image.tsx
@@ -1,6 +1,6 @@
-import { Component, Image } from 'utils';
-
-type Props = {};
+import { classnames, Component, Image, PropsWithClassName } from 'utils';
+import { loadImage } from './image-loader';
+import style from './image.module.scss';
 
 export class ImageComponent extends Component<Props> {
     public constructor(props: Props) {
@@ -8,6 +8,31 @@ export class ImageComponent extends Component<Props> {
     }
 
     public render() {
-        return <Image />;
+        const className = this.createClassName();
+
+        return (
+            <figure className={className}>
+                <Image
+                    src={this.props.src}
+                    alt={this.props.alt}
+                    width="100%"
+                    height="100%"
+                    layout="responsive"
+                    objectFit="cover"
+                    className={this.props.imageClassName ?? ''}
+                    loader={loadImage}
+                    priority
+                />
+            </figure>
+        );
+    }
+    private createClassName(): string {
+        return classnames(style['image-wrapper'], this.props.className);
     }
 }
+
+type Props = PropsWithClassName<{
+    src: string;
+    alt: string;
+    imageClassName?: string;
+}>;
